20827d0ad4abe562ec8abfa1833a5fc0dceeac4a
CHAN YE <qjdrptmzk@naver.com>
2022-07-15 14:32:11 +0900

    vues기초

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/store">Store</router-link> |
+    <router-link to="/store2">Store2</router-link> 
   </nav>
   <router-view/>
 </template>
